0b40a917f22c56143bdceb5a61db4a8af44f75a9
Mohammad Apon <umar426.net@gmail.com>
2023-04-29 16:40:46 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -3,16 +3,36 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors());
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("hello from server")
 })
 
-app.get('/categories',(req,res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
-app.listen(port,()=>{
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectNews = news.find(n => n._id === id)
+    res.send(selectNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+    res.send(categoryNews)
+})
+
+app.listen(port, () => {
     console.log(`coming from server:${port}`)
 })
